5a2ad50781791e14ee58d3dc93411d89d46cd1fe
Ragnar Stefansson <ragnarstefanss@gmail.com>
2022-12-21 01:46:50 +0000

    now useris shown accounts details if he is logged in otherwise he is directed to a login page

---
+++ pages/account/accounts_page.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import LoginPage from '../login';
+
+import Head from "next/head";
+import Header from "../../components/Header";
+import requests from "../../utils/requests";
+import Image from "next/image";
+//import tmdb from "../public/tmdb.svg";
+
+
+export default function AccountsPage({ }) {
+  return (
+    <>
+    <Header />
+   <div><h1>Accounts</h1><p>This is the accounts page.</p></div>
+   </>
+  );
+}
+
--- pages/account/index.js
+++ pages/account/index.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import AccountsPage from './accounts_page';
+import LoginPage from '../login';
+
 import Head from "next/head";
 import Header from "../../components/Header";
 import requests from "../../utils/requests";
@@ -5,12 +10,15 @@ import Image from "next/image";
 //import tmdb from "../public/tmdb.svg";
 
 
-export default function Account({ results }) {
-  return (
-    <>
-    <Header />
-    <h1>Account</h1>
-    </>
-  );
+export default function Account({ }) {
+  const router = useRouter();
+  const isLoggedIn = false; // replace this with your own logic to check if the user is logged in
+
+  if (isLoggedIn) {
+    return <AccountsPage />;
+  } else {
+    router.push('/login');
+  }
+    
 }
 
